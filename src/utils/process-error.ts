7660644a5203eb11a9efdb1f0dc48ceac2255e50
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-29 03:10:44 -0300

    refactor(error): fixed.

--- src/utils/process-error.ts
+++ src/utils/process-error.ts
@@ -1,11 +1,18 @@
+import { AxiosError } from "axios";
+
 interface defaultError {
   message: string;
   statusCode: number;
 }
 
-export function processReponseError({ message }: defaultError): {
-  message: string;
-} {
-  if (!message) return { message: "Ocorreu um erro interno, tente novamente." };
-  return { message };
+type typeError = AxiosError<defaultError>;
+
+export function catchError(err: unknown): string {
+  const error = err as typeError;
+  return processReponseError(error.response?.data.message!);
+}
+
+export function processReponseError(message: string) {
+  if (!message) return "Ocorreu um erro interno, tente novamente.";
+  return message;
 }
