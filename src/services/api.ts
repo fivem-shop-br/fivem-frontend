927aa938184eecd0b4b59fe99bf2e58ea5bd71d6
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-30 07:30:56 -0300

    test in production.

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { parseCookies } from "nookies";
 export function setupApiClient(ctx = undefined) {
   const cookies = parseCookies(ctx)["fivem-shop.token"];
   const api = axios.create({
-    baseURL: "http://191.101.234.50:3000",
+    baseURL: "https://api.fivem.website",
     headers: {
       Authorization: cookies && `Bearer ${cookies}`,
     },
